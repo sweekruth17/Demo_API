31df07bd99034853b872d57ed491157fe398d0fe
Sweekruth K M <55882537+sweekruth17@users.noreply.github.com>
2023-11-18 23:17:46 +0530

    Update connect.js

--- DB/connect.js
+++ DB/connect.js
@@ -1,9 +1,6 @@
 const { default: mongoose } = require("mongoose");
 const mongose = require("mongoose");
 
-// const URL =
-//   "mongodb+srv://sweekruth:ROFNg02uy4TS0Sgs@sweekruth1.u5lfuyh.mongodb.net/sweekruth1?retryWrites=true&w=majority";
-
 const URL = process.env.MONGODB_URL;
 const connectDB = () => {
     console.log("Connecting to mongo DB");
